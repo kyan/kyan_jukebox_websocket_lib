4a37bad62938c23fb6b21463ac84678342091a70
Duncan Robertson <duncan@kyan.com>
2016-01-05 09:36:59 +0000

    Add some methods to playlists and update Gems

--- lib/kyan_jukebox/modules/playlist.rb
+++ lib/kyan_jukebox/modules/playlist.rb
@@ -9,5 +9,22 @@ module KyanJukebox
       replace tracks.map {|track| Track.new(track)} unless tracks.nil?
     end
 
+    def upcoming_tracks
+      current_track_index.nil? ? [] : self[current_track_index...-1]
+    end
+
+    def next_track
+      self[current_track_index+1]
+    end
+
+    def previous_track
+      self[current_track_index-1]
+    end
+
+    private
+
+    def current_track_index
+      @current_track_index ||= index { |track| track.file == current_track }
+    end
   end
 end
